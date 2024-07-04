631b4431994695a4486200c64844148a4ce02ebc
Joël Müller <76882403+muellerjoel@users.noreply.github.com>
2024-07-04 20:20:44 +0200

    Create network.sh

---
+++ network.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# RJ-45 1 GBit/s NIC
+ls-addni dpmac.11
+ls-addni dpmac.12
+ls-addni dpmac.13
+ls-addni dpmac.14
+ls-addni dpmac.15
+ls-addni dpmac.16
+ls-addni dpmac.17
+ls-addni dpmac.18
+# SFP28 25 Gbit/s NIC
+## add it
