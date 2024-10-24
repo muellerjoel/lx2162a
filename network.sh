5444e6ca76f90655ab712eca0b34b4ecffa56cf6
Joël Müller <76882403+muellerjoel@users.noreply.github.com>
2024-10-24 14:15:26 +0200

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# RJ-45 1 GBit/s NIC
+# RJ-45 1 Gbps NIC
 ls-addni dpmac.11
 ls-addni dpmac.12
 ls-addni dpmac.13
@@ -8,5 +8,8 @@ ls-addni dpmac.15
 ls-addni dpmac.16
 ls-addni dpmac.17
 ls-addni dpmac.18
-# SFP28 25 Gbit/s NIC
-## add it
+# SFP+ 10 Gbps and SFP28 25 Gbps NIC
+ls-addni dpmac.3
+ls-addni dpmac.4
+ls-addni dpmac.5
+ls-addni dpmac.6
